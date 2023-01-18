25471547032132cca071f70e7f1d13c882bcddb7
ellafromherz <113733097+ellafromherz@users.noreply.github.com>
2023-01-17 20:02:58 -0500

    ...

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
 const my_name = 'Ella'
 console.log(my_name);
+
+ const colors = ["red", "white", "blue"]
+ console.log(colors);
+
+ const my_team = {
+  name: "Capitals"
+  
+ };
+
+ console.log('my_team name');
