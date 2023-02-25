60ebc0aaab51605bbd68f4a6b92d9ff81941bf9e
emf-99 <113733097+emf-99@users.noreply.github.com>
2023-02-25 14:45:56 -0500

    :art: new calendar java

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
 // @ts-check
+const submitBtn = document.getElementById('submit');
 
-const snake = document.getElementById('snake');
+submitBtn.addEventListener('click', () => {
+  const selectedDate = document.getElementById('datepicker').value;
+  const datePick = new Date(selectedDate);
+  const month = datePick.getMonth() + 1;
+  const day = datePick.getDate();
+
+
+if ((month === 12 && day >= 22) || (month === 1 && day <= 19)) {
+  plant_type = 'succulents'
+} else if ((month === 11 && day >= 22) || (month === 12 && day <= 21)) {
+  plant_type = 'spider plant'
+} else if ((month === 10 && day >= 24) || (month === 11 && day <= 21)) {
+  plant_type = 'black orchid'
+} else if ((month === 9 && day >= 23) || (month === 10 && day <= 23)) {
+  plant_type = 'peace lily'
+} else if ((month === 8 && day >= 23) || (month === 9 && day <= 22)) {
+  plant_type = 'rubber tree'
+} else if ((month === 7 && day >= 23) || (month === 8 && day <= 22)) {
+  plant_type = 'monstera'
+} else if ((month === 6 && day >= 22) || (month === 7 && day <= 22)) {
+  plant_type = 'jade plant'
+} else if ((month === 5 && day >= 21) || (month === 6 && day <= 21)) {
+  plant_type = 'pothos'
+} else if ((month === 4 && day >= 20) || (month === 5 && day <= 20)) {
+  plant_type = 'aloe vera'
+} else if ((month === 3 && day >= 21) || (month === 4 && day <= 19)) {
+  plant_type = 'snake plant'
+} else if ((month === 2 && day >= 19) || (month === 3 && day <= 20)) {
+  plant_type = 'water lily'
+} else if ((month === 1 && day >= 20) || (month === 2 && day <= 18)) {
+  plant_type = 'air plant'
+}
+console.log('your plant type is:', plant_type);
+
+});
+
+
+
+/* const snake = document.getElementById('snake');
 const snakemp3 = document.getElementById('snakemp3')
 const monthSelect = document.getElementById('month');
 const daySelect = document.getElementById('day');
@@ -75,4 +114,4 @@ daySelect.addEventListener('change', function() {
 
 snake.addEventListener('click', function() {
   snakemp3.play();
-});
+}); */
